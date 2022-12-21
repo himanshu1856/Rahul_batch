ff1408591748bf736092acedbdf318dd3b3dd0ed
himanshu1856 <hs287190@gmail.com>
2022-12-21 12:38:06 +0530

    print and delete funtion implemented

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,30 +1,32 @@
-function handlePrint(text){
-    //base case
+function handlePrint(){
     if(index == text.length){
-        // handleDelete()
+        handleDelete()
         return
     }
-    // I am a CS student
-    let character = text[index]; // I 
-    index++;
-    content += character;
+    let char = text[index]
+    content += char
     dynamicSpan.textContent = content
-    setTimeout(handlePrint,300,text)
+    index++
+    setTimeout(handlePrint,300)
 }
 
-// function handleDelete(){
-//     if(index == -1){
-//         return
-//     }
-//     index--
-//     content = content.substring(0,index-1)
-//     dynamicSpan.textContent = content
-//     setTimeout(handleDelete,300,text)
-// }
+// index = 15
+// content = "I am a CS student"
+
+function handleDelete(){
+    if(index == -1){
+        return
+    }
+    index--;
+    let ss = content.substring(0,index-1)
+    content = ss
+    dynamicSpan.textContent = content
+    setTimeout(handleDelete,300)
+}
 
 const dynamicSpan = document.querySelector("#dynamic-text")
 
-const phrases = ["I am a CS student","I am Software Engineer"]
+const phrases = ["I am a CS student...","I am Software Engineer"]
 let text = phrases[0]
 let index = 0
 let content = ''
